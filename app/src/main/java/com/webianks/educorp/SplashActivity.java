565c658daf29b3761122045bb774d79a96aec14c
webianks <webianks@gmail.com>
2017-01-14 14:00:34 +0530

    Initial commit

---
+++ app/src/main/java/com/webianks/educorp/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.webianks.educorp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                showLoginRegister();
+            }
+        }, 1000);
+
+    }
+
+    private void showLoginRegister() {
+        startActivity(new Intent(this, LoginRegisterActivity.class));
+        finish();
+    }
+}
